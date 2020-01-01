a53a3b083c2147b52971389a7c6aca3df8a45757
priyamvada <pranshulpandey5@gmail.com>
2020-01-01 19:19:49 +0000

    adding the rental list, rental-list-item, rental-detail, and few more rentals

---
+++ src/app/rental/rental.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rental',
+  templateUrl: './rental.component.html',
+  styleUrls: ['./rental.component.scss']
+})
+export class RentalComponent {
+  //title = 'bwm-angular-cli';
+}
---
+++ src/app/rental/rental.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RentalComponent} from './rental.component';
+import { RentalListComponent } from './rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
+import { RentalService} from './shared/rental.services';
+import { Routes, RouterModule} from '@angular/router';
+import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+
+const routes: Routes=[
+{path:'rental', 
+component: RentalComponent,
+children: [
+{ path: '', component: RentalListComponent},
+{ path: ':rental_ID', component: RentalDetailComponent}
+]
+}
+
+]
+
+@NgModule({
+  declarations: [
+    RentalComponent,
+    RentalListComponent,
+    RentalListItemComponent,
+    RentalDetailComponent
+
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+
+  providers: [RentalService]
+})
+export class RentalModule {
+//console.log('Hi this is rental Module');
+ }
